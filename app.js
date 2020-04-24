1565aa37dc4605ec4b05d436554a8af4a2e41565
karl.hudgell <karl.hudgell@bjss.com>
2020-04-24 22:20:41 +0100

    working wifi connect

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
-const common = require('./lib/common');
-const inquirer = require('./lib/inquirer');
-const connectWifi = require('./pages/pages')
+const pages = require('./pages/pages')
 
-
-const mainMenu = async () => {
-    common.header()
-    console.log(chalk.blue('Main Menu'))
-    console.log(chalk.red('----------'))
-    const mainMenuSelection = await inquirer.mainMenu();
-    console.log(mainMenuSelection);
-    switch(mainMenuSelection.mainMenu) {
-        case 'connect to miwatch via wifi':
-        connectWifi.connectWifi()
-        break;
-        case 'y':
-          // code block
-          break;
-        default:
-          // code block
-      }
-};
-
-mainMenu();
-
-
-// if (files.directoryExists('.git')) {
-//     console.log(chalk.red('Already a Git repository!'));
-//     process.exit();
-// }
+pages.mainMenu();
--- pages/pages.js
+++ pages/pages.js
@@ -3,7 +3,8 @@ const clear = require('clear');
 const figlet = require('figlet');
 const common = require('../lib/common');
 const inquirer = require('../lib/inquirer');
-const adb = require('node-adb');
+const shellExec = require('shell-exec')
+const mainMenu = require('../app')
 
 
 module.exports = {
@@ -13,11 +14,34 @@ module.exports = {
         console.log(chalk.red('----------'))
 
         const miWatchIpaddress = await inquirer.connectWifi();
-        console.log(miWatchIpaddress.connectWifi)
-        adb({
-            cmd: ['devices']
-        }, function (result) {
-            console.log(result)
-        });
-    }
+        shellExec('adb connect ' + miWatchIpaddress.connectWifi).then(async function (result) {
+            if (result.stdout.includes('unable to connect')){
+                console.log(chalk.red('MiWatch not found'))
+                // await new Promise(resolve => setTimeout(resolve, 5000));
+                await common.pause(3000)
+                module.exports.connectWifi()
+            } else {
+                console.log(chalk.green('MiWatch Connected'))
+                await common.pause(3000)
+                module.exports.mainMenu()
+            }
+        }).catch()
+    },
+    mainMenu: async () => {
+        common.header()
+        console.log(chalk.blue('Main Menu'))
+        console.log(chalk.red('----------'))
+        const mainMenuSelection = await inquirer.mainMenu();
+        console.log(mainMenuSelection);
+        switch (mainMenuSelection.mainMenu) {
+          case 'connect to miwatch via wifi':
+              module.exports.connectWifi()
+            break;
+          case 'y':
+            // code block
+            break;
+          default:
+            // code block
+        }
+      }
 };
