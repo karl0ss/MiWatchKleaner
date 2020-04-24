1f11fe1c4a382a23f39067811204502411854f13
karl.hudgell <karl.hudgell@bjss.com>
2020-04-24 22:55:00 +0100

    latest

--- pages/pages.js
+++ pages/pages.js
@@ -1,23 +1,17 @@
 const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
 const common = require('../lib/common');
 const inquirer = require('../lib/inquirer');
 const shellExec = require('shell-exec')
-const mainMenu = require('../app')
 
 
 module.exports = {
     connectWifi: async () => {
-        common.header()
-        console.log(chalk.blue('Connect Wifi'))
-        console.log(chalk.red('----------'))
-
+        common.header('Connect Wifi')
         const miWatchIpaddress = await inquirer.connectWifi();
-        shellExec('adb connect ' + miWatchIpaddress.connectWifi).then(async function (result) {
-            if (result.stdout.includes('unable to connect')){
+        this.miWatchIpaddress = miWatchIpaddress.connectWifi
+        shellExec('adb connect ' + miWatchIpaddress).then(async function (result) {
+            if (result.stdout.includes('unable to connect')) {
                 console.log(chalk.red('MiWatch not found'))
-                // await new Promise(resolve => setTimeout(resolve, 5000));
                 await common.pause(3000)
                 module.exports.connectWifi()
             } else {
@@ -28,20 +22,17 @@ module.exports = {
         }).catch()
     },
     mainMenu: async () => {
-        common.header()
-        console.log(chalk.blue('Main Menu'))
-        console.log(chalk.red('----------'))
+        common.header('Main Menu')
         const mainMenuSelection = await inquirer.mainMenu();
-        console.log(mainMenuSelection);
         switch (mainMenuSelection.mainMenu) {
-          case 'connect to miwatch via wifi':
-              module.exports.connectWifi()
-            break;
-          case 'y':
-            // code block
-            break;
-          default:
-            // code block
+            case 'connect to miwatch via wifi':
+                module.exports.connectWifi()
+                break;
+            case 'y':
+                // code block
+                break;
+            default:
+                // code block
         }
-      }
+    }
 };
