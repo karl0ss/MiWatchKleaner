b00973036b89720cac50d2fb963aadcca0b3f094
karl.hudgell <karl.hudgell@bjss.com>
2020-04-25 10:22:47 +0100

    working code update

--- pages/pages.js
+++ pages/pages.js
@@ -7,15 +7,24 @@ const shellExec = require('shell-exec')
 module.exports = {
     connectWifi: async () => {
         common.header('Connect Wifi')
-        const miWatchIpaddress = await inquirer.connectWifi();
-        this.miWatchIpaddress = miWatchIpaddress.connectWifi
+        const value = await inquirer.connectWifi();
+        const miWatchIpaddress = value.connectWifi
         shellExec('adb connect ' + miWatchIpaddress).then(async function (result) {
             if (result.stdout.includes('unable to connect')) {
                 console.log(chalk.red('MiWatch not found'))
-                await common.pause(3000)
+                await common.pause(2000)
+                console.log(chalk.white('Try Again'))
+                await common.pause(1000)
+                module.exports.connectWifi()
+            } else if (result.stdout.includes('cannot connect')) {
+                console.log(chalk.red('MiWatch not found'))
+                await common.pause(2000)
+                console.log(chalk.white('Try Again'))
+                await common.pause(1000)
                 module.exports.connectWifi()
             } else {
                 console.log(chalk.green('MiWatch Connected'))
+                common.writeIpAddress(miWatchIpaddress)
                 await common.pause(3000)
                 module.exports.mainMenu()
             }
